033255c12bee34f60e4cc594809dfa79429b46d4
Akitaka Ito <a2ki0ta0ka0@icloud.com>
2021-12-16 21:53:57 +0900

    SideMenuの練習用

--- EasySideMenu/ViewController.swift
+++ EasySideMenu/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func tappedMenu(_ sender: UIBarButtonItem) {
+        self.performSegue(withIdentifier: "showMenu", sender: nil)
+    }
+    
 
 }
 
